1d8fd925b5d1ce572a55c27fc5163e79f43249dc
PeirisWRL <99251745+PeirisWRL@users.noreply.github.com>
2022-02-08 16:54:28 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 
 int main() {
+   int x, y;
+int sum;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+sum = x + y;
+cout << "Sum is: " << sum;
   
   return 0;
 }
